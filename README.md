1bd7d1502f6b7c9b3e45f0b64d012ace160a9a09
Magnus Dyre-Moe <magnus.dyremoe@gmail.com>
2020-11-14 18:23:00 +0100

    clean

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # caltech101
 Image classificiation on data set caltech101
 
-Data set caltech101 is split into 101 different classes. Run split_train_test.py to generate train and test set. This requires one folder named train, containt all images, and one empty folder named val.
+Data set caltech101 is split into 101 different classes. Run split_train_test.py to generate train, validation and test set. This requires one folder named train, containt all images, one empty folder named val and one folder named test.
 The caltech101 data set can be found at https://www.kaggle.com/athota1/caltech101 .
 
 Virtual machine made to run on Google cloud. For tutorial on how to run the vm follow this link: https://www.youtube.com/watch?v=Db4FfhXDYS8&ab_channel=JinayShah .
--- data_visuals.py
+++ data_visuals.py
@@ -2,6 +2,8 @@
 
 import os
 import matplotlib.pyplot as plt
+import random
+import cv2
 
 base_path = os.getcwd()
 data_path = os.path.join(base_path, "101_ObjectCategories/train")
@@ -29,8 +31,42 @@ for cat in categories_val:
     image_files = os.listdir(os.path.join(val_path, cat))
     images_in_class[cat] += len(image_files)
 
+"""
+# Display number of images per object category
 plt.figure(figsize=(12,5))
 plt.bar(range(len(images_in_class)), list(images_in_class.values()), align='edge', width=0.8)
 plt.xticks(range(len(images_in_class)), list(images_in_class.keys()), rotation='vertical', fontsize=8)
 plt.tight_layout()
 plt.show()
+"""
+
+
+# Visualize images in 4 randomly selected classes.
+num_categories = 4
+num_images = 4
+
+random_categories = random.sample(list(images_in_class), num_categories)
+print(random_categories)
+
+fig, ax = plt.subplots(nrows=num_categories, ncols=num_images)
+
+for i, category in enumerate(random_categories):
+    folder_path = os.path.join(data_path, category)
+    images = [img for img in os.listdir(folder_path)][:num_images]
+    for j, image in enumerate(images):
+        file_path = os.path.join(folder_path, image)
+        selected_image = cv2.imread(file_path)
+        selected_image = cv2.resize(selected_image, (100,100))
+        selected_image = cv2.cvtColor(selected_image, cv2.COLOR_BGR2RGB)
+        ax[i,j].imshow(selected_image)
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
+
+        if j==0:
+            ax[i,j].annotate(category, xy=(0, -0.5), xycoords=ax[i,j].yaxis.label, 
+                            size='large', ha='right', va='center')
+
+fig.tight_layout()
+fig.show()
+plt.show()
+
